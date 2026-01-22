8cb44af81c9a068d4e19dd9054d5d1bf4fa3ee97
mac <mac@macs-MacBook-Air.local>
2026-01-21 20:06:13 -0800

    Fix CORS and MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,52 +1,80 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const UserModel = require('./models/Users');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require("./models/Users");
 
 const app = express();
+
 app.use(express.json());
-app.use(cors({
-  origin: ["https://crud-inky-eight.vercel.app"],
-  methods: ["POST", "GET", "PUT", "DELETE" ],
-  credentials: true
-}));
 
-mongoose.connect('mongodb+srv://crud:crud1234@cluster0.fa5yt.mongodb.net/users');
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3002",
+      "https://crud-inky-eight.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
-app.get('/', (req, res) => {
-  UserModel.find({})
-    .then(users => res.json(users))
-    .catch(err => res.json(err));
+app.options("*", cors());
+
+mongoose
+  .connect(
+    "mongodb+srv://crud:crud1234@cluster0.vuhnufg.mongodb.net/?appName=Cluster0",
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB error:", err));
+
+app.get("/", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-app.post('/createUser', (req, res) => {
-  UserModel.create(req.body)
-    .then(user => res.json(user))
-    .catch(err => res.json(err));
+app.post("/createUser", async (req, res) => {
+  try {
+    const user = await UserModel.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-
-app.get('/users/:id', (req, res) => {
-  const id = req.params.id;
-  UserModel.findById(id)
-    .then(user => res.json(user))
-    .catch(err => res.json(err));
+app.get("/users/:id", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.id);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-app.put('/updateUser/:id', (req, res) => {
-  const id = req.params.id;
-  UserModel.findByIdAndUpdate(id, { name: req.body.name, age: req.body.age, email: req.body.email }, { new: true })
-    .then(user => res.json(user))
-    .catch(err => res.json(err));
+app.put("/updateUser/:id", async (req, res) => {
+  try {
+    const user = await UserModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-app.delete('/deleteUser/:id', (req, res) => {
-  const id = req.params.id;
-  UserModel.findByIdAndDelete(id)
-    .then(user => res.json(user))
-    .catch(err => res.json(err));
+app.delete("/deleteUser/:id", async (req, res) => {
+  try {
+    const user = await UserModel.findByIdAndDelete(req.params.id);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 app.listen(3001, () => {
-  console.log('Server is running');
+  console.log("Server is running on port 3001");
 });
